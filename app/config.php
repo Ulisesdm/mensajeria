293433bb809283f5896b577edb5953170aadcd91
Ulises Dominguez Medina <135391703+Ulisesdm@users.noreply.github.com>
2025-01-06 17:36:42 -0600

    Actualizar config.php

--- app/config.php
+++ app/config.php
@@ -11,7 +11,7 @@
 	define('ROOT', "http://localhost/html5/Mensajeria/"); #representa [ ruta absoluta de la carpeta ]
 	define('URL', "http://localhost/html5/Mensajeria/"); #representa [ ruta web ]
 
-	#5e:Pruebas de Correo Aiko
+	#5e:Pruebas de Correo
 	define('MSERVER', "com.mx"); #Host servidor de correos [ pruebas ]
 	define('MPSERVER', 587);        	   #Puerto servidor de correos [ pruebas ]
 	define('CFSERVER', " "); 			   #Cifrado servidor de correos [ pruebas ] [PRO : ", TLS"]
